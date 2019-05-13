09b195bad157e7f3c743d4ca5c20163000753ce1
Roman Spodaryk <roman.spodaryk@ukr.net>
2019-05-13 18:16:17 +0300

    created base for cashier app

---
+++ app/src/main/java/com/example/android/noq_cashier/fragments/NewOrdersFragment.java
@@ -0,0 +1,49 @@
+package com.example.android.noq_cashier.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.noq_cashier.R;
+
+
+
+public class NewOrdersFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private View view;
+
+
+    public static NewOrdersFragment newInstance(String param1, String param2) {
+        NewOrdersFragment fragment = new NewOrdersFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_list_of_new_orders, container, false);
+
+        return view;
+    }
+
+
+}
