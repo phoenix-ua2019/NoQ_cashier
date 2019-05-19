e26bd6c18ab5e840e93da36b79865c2bfeb6c5c3
hudanov <scalesymmetry@gmail.com>
2019-05-19 17:28:42 +0300

    Confirmation dialog window added

--- app/src/main/java/ua/lviv/iot/phoenix/noq_cashier/fragments/OrderFragment.java
+++ app/src/main/java/ua/lviv/iot/phoenix/noq_cashier/fragments/OrderFragment.java
@@ -1,6 +1,7 @@
 package ua.lviv.iot.phoenix.noq_cashier.fragments;
 
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -26,6 +27,10 @@ public class OrderFragment extends Fragment {
 
     Button acceptOrder;
     Button rejectOrder;
+    Button confirm;
+    Button decline;
+
+    Dialog confirmationDialog;
 
     BaseActivity currentActivity;
 
@@ -59,11 +64,7 @@ public class OrderFragment extends Fragment {
         acceptOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Bundle b = new Bundle();
-                order.setStatus(true);
-                b.putParcelable("accepted order", order);
-                setArguments(b);
-                currentActivity.goToAcceptedOrderFragment(view);
+                confirmationDialogCaller(order, true);
             }
         });
 
@@ -72,16 +73,55 @@ public class OrderFragment extends Fragment {
             public void onClick(View v) {
                 //
                 //setArguments(b);
-                order.setStatus(false);
-                currentActivity.goToNewOrdersFragment(view);
+                confirmationDialogCaller(order, false);
             }
         });
 
-
-
-
-
         return view;
+    }
+
+    public void confirmationDialogCaller(Order order, boolean status) {
+        confirmationDialog = new Dialog(currentActivity);
+        confirmationDialog.setContentView(R.layout.acception_dialog);
+
+        confirm = confirmationDialog.findViewById(R.id.confirm_button);
+        decline = confirmationDialog.findViewById(R.id.decline_button);
+        TextView confirmationMassage = confirmationDialog.findViewById(R.id.confirmation_message);
+
+        confirm.setEnabled(true);
+        decline.setEnabled(true);
+
+        if (status) {
+            confirmationMassage.setText("Ви впевнені, що хочете підтвердити замовлення?");
+            confirm.setOnClickListener((View v) -> {
+                Bundle b = new Bundle();
+                order.setStatus(true);
+                b.putParcelable("accepted order", order);
+                setArguments(b);
+                currentActivity.goToAcceptedOrderFragment(view);
+                confirmationDialog.cancel();
+            });
+
+            decline.setOnClickListener((View v) -> {
+                confirmationDialog.cancel();
+            });
+
+        } else {
+            confirmationMassage.setText("Ви впевнені, що хочете відхилити замовлення?");
+            confirm.setOnClickListener((View v) -> {
+                order.setStatus(false);
+                currentActivity.goToNewOrdersFragment(view);
+                confirmationDialog.cancel();
+            });
+
+            decline.setOnClickListener((View v) -> {
+                confirmationDialog.cancel();
+            });
+
+        }
+
+
+        confirmationDialog.show();
     }
 
 }
